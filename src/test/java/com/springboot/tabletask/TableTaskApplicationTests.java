c0bdebdc272e70f752d107858a78c498f6f97a50
seohyeon <uimi3606@gmail.com>
2024-05-04 21:28:48 +0900

    init

---
+++ src/test/java/com/springboot/tabletask/TableTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.tabletask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
